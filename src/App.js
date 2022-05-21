7e72a3463120044604cf5b250256fe57b7d47cec
Sabir Hassan <sabirbk06@gmail.com>
2022-05-22 00:06:46 +0500

    feat: add sidebar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState } from "react"
+import { Box, createTheme, Stack, ThemeProvider } from "@mui/material"
 import Navbar from "./components/Navbar"
+import Sidebar from "./components/Sidebar"
 
 function App() {
-  return <Navbar />
+  const [mode, setMode] = useState("light")
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  })
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar setMode={setMode} mode={mode} />
+        </Stack>
+      </Box>
+    </ThemeProvider>
+  )
 }
 
 export default App
